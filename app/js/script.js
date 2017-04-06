8f8431933c45b1a42b8958b533782f94aa58e44b
Denis <corvus007@yandex.ru>
2017-04-06 16:05:59 +0300

    Создает скрипты для карты и модального окна

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   svg4everybody(); // run it now or whenever you are ready
 
+  // Загрузка карты
+  loadMapScript()
+
   /*=================================
   =            Billboard            =
   =================================*/
@@ -18,13 +21,63 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+  /*====================================
+  =            Inline popup            =
+  ====================================*/
+
+  $('.js-trigger-inline-popup').magnificPopup({
+    mainClass: 'popup-fade',
+    removalDelay: 300
+  });
+
+  /*=====  End of Inline popup  ======*/
+
+
+
   /*==================================
   =            Input mask            =
   ==================================*/
 
-  // Phone
   $('input[type="tel"]').mask("+7 (999) 999-99-99", {});
 
   /*=====  End of Input mask  ======*/
 
 });
+
+
+// Загрузка карты
+function loadMapScript() {
+  var script = document.createElement("script");
+  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyC9a_UDjprd--E33HE4d9_S6I0yjvViR8o&callback=initializeMap";
+  document.head.appendChild(script);
+}
+
+// Инициализация карты
+function initializeMap() {
+  var locationOffice = {
+    lat: 52.790924,
+    lng: 52.265363
+  };
+
+  function createProp(defaultLocation) {
+    return {
+      center: defaultLocation,
+      zoom: 15,
+      streetViewControl: false,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_CENTER
+      },
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      scrollwheel: false,
+    };
+  }
+
+  var mapProp = createProp(locationOffice);
+  var map = new google.maps.Map(document.getElementById("contacts-map"), mapProp);
+  var markerPriem = new google.maps.Marker({
+    position: locationOffice,
+    map: map,
+    title: 'Современная стоматология Дикман'
+  });
+}
